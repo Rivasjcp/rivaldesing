a0d1ab6f365d48b64778553ffd3e5fc9e44cc150
Jhan Rivas <jcrivas1998@gmail.com>
2022-05-28 15:25:29 -0400

    FinalWeb

---
+++ js/funciones.js
@@ -0,0 +1,16 @@
+
+ // Boton de ir hacia arriba 
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
